8d63e59d5af56c635b171c16e707bbd50704aa9b
BruceF1 <bruce.farthing@gmail.com>
2021-09-29 08:57:02 +0200

    272 Added restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
-  resources :user_stocks
+  resources :user_stocks, only: [:create]
 end
